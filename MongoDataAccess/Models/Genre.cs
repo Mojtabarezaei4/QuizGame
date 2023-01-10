b0522a69c6294bf6ad882d52a9426f39e13cc80c
Mojtabarezaei4 <mojtabarezaei2001@gmail.com>
2023-01-10 20:20:10 +0100

    Delete MongoDataAccess folder

--- MongoDataAccess/Models/Genre.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace MongoDataAccess.Models;
-
-public class Genre
-{
-    
-}
--- MongoDataAccess/Models/IQuestion.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MongoDataAccess.Models;
-
-public interface IQuestion
-{
-        public string Statement { get; set; }
-        public string? ImageSource { get; set; }
-        public string[] Answers { get; set; }
-        public int CorrectAnswer { get; set; }
-}
